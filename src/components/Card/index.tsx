2673af0e22f03c0a7930a551bd94387a5a96065c
Bima Saputra <109858732+bmsptra24@users.noreply.github.com>
2024-10-13 12:53:34 +0700

    fix: card responsive

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -14,10 +14,10 @@ const Card: React.FC<CardProps> = ({ id, title, description, imageCover }) => {
   return (
     <Link
       to={"/article/" + id}
-      className="flex cursor-pointer justify-between gap-20"
+      className="flex cursor-pointer flex-col items-center justify-between gap-2 md:flex-row md:gap-20"
     >
       {/* Text Content */}
-      <div className="flex flex-col gap-4 p-8">
+      <div className="flex flex-col gap-4 md:p-8">
         <Text variant="subtitle">{title}</Text>
         <Text variant="description" className="line-clamp-5">
           {description}
@@ -28,7 +28,7 @@ const Card: React.FC<CardProps> = ({ id, title, description, imageCover }) => {
       <img
         src={imageCover}
         alt="article-cover"
-        className="aspect-video h-fit w-[30rem] rounded-3xl object-cover"
+        className="aspect-video h-fit w-full rounded-3xl object-cover md:w-[20rem] lg:w-[30rem]"
       />
     </Link>
   );
